6bb2138c1187eba2efbe539905eb0c858b1d9abc
wlw1106 <53527516+wlw1106@users.noreply.github.com>
2022-07-03 11:06:58 +0800

    Add files via upload

---
+++ networking_sim.ino
@@ -0,0 +1,77 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "Lik Wai的iPhone";
+const char* password = "00000000";
+
+#define TOKEN "0ltoOWWpxAltNsyXWQpA"
+
+int number;
+
+char ThingsboardHost[] = "demo.thingsboard.io";
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+int status = WL_IDLE_STATUS;
+
+void setup() {
+  pinMode(5, INPUT); //+
+  pinMode(4, INPUT); //-
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(200);
+    Serial.print(".");
+  } Serial.println("");
+  Serial.print("connected to");
+  Serial.println(ssid);
+  client.setServer(ThingsboardHost, 1883);
+}
+
+void loop() {
+  if ( !client.connected() ) {
+    reconnect();
+  }
+  if (digitalRead(5) == LOW) {
+    number += 1;
+    getAndSendData();
+    delay(500);
+  } else if (digitalRead(4) == LOW) {
+    number -= 1;
+    getAndSendData();
+    delay(500);
+  }
+}
+
+void getAndSendData() { // Prepare a JSON payload string
+  String payload = "{";
+  payload += "\"Number\":";
+  payload += number;
+  /*payload += ",";
+  payload += "\"Temperature\":";
+  payload += 25;*/
+  payload += "}";
+  char attributes[1000];
+  payload.toCharArray( attributes, 1000 );
+  client.publish("v1/devices/me/telemetry", attributes);
+  Serial.println(attributes);
+}
+
+void reconnect() { // Loop until we're reconnected
+  while (!client.connected()) {
+    status = WiFi.status();
+    if ( status != WL_CONNECTED) {
+      WiFi.begin(ssid, password);
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+      } Serial.println("Connected to AP");
+    } Serial.print("Connecting to ThingsBoard node ..."); // Attempt to connect (clientId, username, password)
+    if ( client.connect("Esp8266", TOKEN, NULL) ) {
+      Serial.println( "[DONE]" );
+    } else {
+      Serial.print( "[FAILED] [ rc = " );
+      Serial.println( " : retrying in 5 seconds]" );
+      delay(500);
+    }
+  }
+}
